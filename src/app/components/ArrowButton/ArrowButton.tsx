a21408d088302c6a4464b1b86182cc77a86321cb
Jan <janhendrikstruve@gmail.com>
2021-11-22 19:12:16 +0100

    Add ArrowButton Ccomponent

---
+++ src/app/components/ArrowButton/ArrowButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+import { ArrowDown, ArrowUp } from '../Icons/Arrow'
+
+export default function ArrowButton({
+  direction,
+}: {
+  direction: string
+}): JSX.Element {
+  return (
+    <StyledButton>
+      {direction === 'up' ? <ArrowUp /> : <ArrowDown />}
+    </StyledButton>
+  )
+}
+
+const StyledButton = styled.button``
