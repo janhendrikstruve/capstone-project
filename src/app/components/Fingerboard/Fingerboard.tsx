b6cf55c3916de31ccf02e1701985a9286aaa40e4
Jan <janhendrikstruve@gmail.com>
2021-11-24 16:38:48 +0100

    Finish basic guitar fingerboard styling

--- src/app/components/Fingerboard/Fingerboard.tsx
+++ src/app/components/Fingerboard/Fingerboard.tsx
@@ -9,7 +9,9 @@ export default function Fingerboard(): JSX.Element {
       {board.map((string) => (
         <StyledSpan>
           {string.map(() => (
-            <StyledDiv></StyledDiv>
+            <StyledDiv>
+              <Guitarstring></Guitarstring>
+            </StyledDiv>
           ))}
         </StyledSpan>
       ))}
@@ -18,15 +20,34 @@ export default function Fingerboard(): JSX.Element {
 }
 
 const StyledSection = styled.section`
+  &:after {
+    content: ' ';
+    position: absolute;
+    z-index: +11;
+    top: 2px;
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
+    border-top: 3px solid brown;
+  }
+  position: relative;
   display: flex;
   flex-direction: row;
-  border: 2px solid brown;
+  border: 3px solid brown;
   width: fit-content;
+  background-color: #202020;
 `
 
-const StyledSpan = styled.span``
+const StyledSpan = styled.span`
+  display: grid;
+  grid-gap: 2px;
+`
 
 const StyledDiv = styled.div`
-  padding: 20px 10px;
-  border-top: 2px solid black;
+  padding: 0 10px;
+  background-color: #ffddbd;
+`
+const Guitarstring = styled.div`
+  padding: 20px 0;
+  border: 1px solid #b47601;
 `
