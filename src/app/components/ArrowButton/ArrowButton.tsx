bc8ec7133987b4f0dc0468dd01ae2632bdffb5fb
Jan <janhendrikstruve@gmail.com>
2021-11-29 18:19:50 +0100

    Update arrow icons import path

--- src/app/components/ArrowButton/ArrowButton.tsx
+++ src/app/components/ArrowButton/ArrowButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { ArrowDown, ArrowUp } from '../../icons/Arrow'
+import { ArrowDown, ArrowUp } from '../../Icons/Arrows'
 
 export default function ArrowButton({
   direction,
