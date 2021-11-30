0986cedd0b14dce7e3ac4bd8bd39317ccd7c764c
Jan <janhendrikstruve@gmail.com>
2021-11-30 14:57:08 +0100

    Create rclick event and render function for icons on fingerboard

--- src/app/components/Fingerboard/Fingerboard.tsx
+++ src/app/components/Fingerboard/Fingerboard.tsx
@@ -1,32 +1,69 @@
 import React from 'react'
 import styled from 'styled-components'
+import { IconArray } from '../../Icons/IconArray'
 
-export default function Fingerboard(): JSX.Element {
-  const board = Array(6).fill(Array(20).fill(null))
+type fingerboardProps = {
+  handleClick?: (columnIndex: number, rowIndex: number) => void
+  pressed: { e: number; a: number; d: number; g: number; b: number; e2: number }
+}
+export default function Fingerboard({
+  handleClick,
+  pressed,
+}: fingerboardProps): JSX.Element {
+  const board = Array(6).fill(Array(4).fill(null))
 
+  function renderClicked(columnIndex: number, rowIndex: number): JSX.Element {
+    rowIndex++
+    if (columnIndex === 0 && rowIndex === pressed.e) {
+      return <PressIcon />
+    }
+    if (columnIndex === 1 && rowIndex === pressed.a) {
+      return <PressIcon />
+    }
+    if (columnIndex === 2 && rowIndex === pressed.d) {
+      return <PressIcon />
+    }
+    if (columnIndex === 3 && rowIndex === pressed.g) {
+      return <PressIcon />
+    }
+    if (columnIndex === 4 && rowIndex === pressed.b) {
+      return <PressIcon />
+    }
+    if (columnIndex === 5 && rowIndex === pressed.e2) {
+      return <PressIcon />
+    } else {
+      return <></>
+    }
+  }
   return (
-    <StyledSection>
-      {board.map((string) => (
-        <YPosition>
-          {string.map(() => (
-            <XYPosition>
+    // <Heading children='add chord'/>
+    <Board>
+      {board.map((column, columnIndex) => (
+        <YPosition key={columnIndex}>
+          {column.map((_checked: boolean, rowIndex: number) => (
+            <XYPosition
+              key={rowIndex}
+              onClick={() => {
+                if (handleClick) handleClick(columnIndex, rowIndex)
+              }}
+            >
               <Guitarstring></Guitarstring>
+              {renderClicked(columnIndex, rowIndex)}
             </XYPosition>
           ))}
         </YPosition>
       ))}
-    </StyledSection>
+    </Board>
   )
 }
 
-const StyledSection = styled.section`
+const Board = styled.section`
   &:after {
     content: ' ';
     position: absolute;
     top: 2px;
     left: 0px;
     right: 0px;
-    bottom: 0px;
     border-top: 3px solid brown;
   }
   position: relative;
@@ -45,8 +82,18 @@ const YPosition = styled.span`
 const XYPosition = styled.div`
   padding: 0 10px;
   background-color: #ffddbd;
+  position: relative;
 `
 const Guitarstring = styled.div`
   padding: 20px 0;
   border: 1px solid #b47601;
+`
+const PressIcon = styled(
+  IconArray[Math.floor(Math.random() * IconArray.length)]
+)`
+  position: absolute;
+  z-index: 100;
+  left: 0;
+  right: 0;
+  top: 0.6rem;
 `
