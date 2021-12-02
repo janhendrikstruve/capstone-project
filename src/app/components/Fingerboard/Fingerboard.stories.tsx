cada25e4949ffa6ca885d240c647d487f19ec67b
Jan <janhendrikstruve@gmail.com>
2021-12-02 17:53:01 +0100

    Fix Fingerboard stories, so it provides all arguments needed

--- src/app/components/Fingerboard/Fingerboard.stories.tsx
+++ src/app/components/Fingerboard/Fingerboard.stories.tsx
@@ -6,4 +6,9 @@ export default {
   component: Fingerboard,
 }
 
-export const standard = (): JSX.Element => <Fingerboard />
+export const empty = (): JSX.Element => (
+  <Fingerboard pressed={{ e2: 0, b: 0, g: 0, d: 0, a: 0, e: 0 }} offset={0} />
+)
+export const firstRowFilled = (): JSX.Element => (
+  <Fingerboard pressed={{ e2: 1, b: 1, g: 1, d: 1, a: 1, e: 1 }} offset={0} />
+)
