086d6bdd3a9b5a577b87ac9dc5790bef9e1e470c
Jan <janhendrikstruve@gmail.com>
2021-12-03 15:31:06 +0100

    Do some minor fixes: Change closing tag to selfclosing, changed rem mesurement to px mesurment for consistancy

--- src/app/Pages/Dashboard/Dashboard.tsx
+++ src/app/Pages/Dashboard/Dashboard.tsx
@@ -4,7 +4,7 @@ import Fingerboard from '../../components/Fingerboard/Fingerboard'
 import { fingerboardData } from '../../data/fingerboardData/fingerboardData'
 import Heading from '../../components/Heading/Heading'
 import styled from 'styled-components'
-import Fret from '../../components/Fret/Fret'
+import FretCounter from '../../components/Fret/FretCounter'
 import ArrowButton from '../../components/ArrowButton/ArrowButton'
 
 export default function Dashboard(): JSX.Element {
@@ -20,7 +20,6 @@ export default function Dashboard(): JSX.Element {
     if (column === 3) newPressed.g = pressedFret
     if (column === 4) newPressed.b = pressedFret
     if (column === 5) newPressed.e2 = pressedFret
-    console.log(newPressed)
     setPressed(newPressed)
   }
 
@@ -45,7 +44,7 @@ export default function Dashboard(): JSX.Element {
             direction={true}
             handleClick={handleFretOffset}
           ></ArrowButton>
-          <StyledFret start={fretOffset + 1} end={fretOffset + 4}></StyledFret>
+          <FretCounter start={fretOffset + 1} end={fretOffset + 4} />
           <ArrowButton
             direction={false}
             handleClick={handleFretOffset}
@@ -60,14 +59,12 @@ const StyledMain = styled.main`
   display: grid;
   justify-items: center;
   grid-template-rows: auto auto;
-  grid-gap: 1rem;
+  grid-gap: 16px;
 `
 
 const FingerboardFunctions = styled.div`
   display: flex;
 `
-
-const StyledFret = styled(Fret)``
 
 const Arrows = styled.div`
   display: flex;
