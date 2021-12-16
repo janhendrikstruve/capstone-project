939aa31eba481a70bf4c1ed2c418ff3d06a22406
Jan <janhendrikstruve@gmail.com>
2021-12-16 12:10:24 +0100

    Create unfinished chrod folder. Dev: amend when finished

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './Pages/Dashboard/Dashboard'
+import ChordFolder from './Pages/ChordFolder/ChordFolder.tsx'
 
 export default function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/chordfolder" element={<ChordFolder />} />
       </Routes>
     </BrowserRouter>
   )
